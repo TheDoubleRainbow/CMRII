0357018971e5aeee0cf2e7941dd978a406b8b409
TheDoubleRainbow <16mb.com@gmail.com>
2018-02-18 18:23:43 +0200

    angular version and locales

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,19 +1,28 @@
-var app = new Vue({
-	el: "#app",
-	data: {
-		alternatives: [/*{name: "Файловий менеджер", rate: 0}, {name: "Браузер", rate: 0}, {name: "Сапер", rate: 0}, {name: "Поштовий клієнт", rate: 0}, {name: "Калькулятор", rate: 0}*/],
-		alternativesList: [],
-		currentCompare: [],
-		currentRange: 5,
-		currentFirst: undefined,
-		currentIteration: 0,
-		outOfAlternatives: false,
-		newAlternative: '',
-		resultList: [],
-		ready: false
-	},
-	methods: {
-		getCurrentCompare: function(){
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+	locales = {
+		en: {lang: "Language", inputTitle: 'Add your alternatives', add: 'Add', ready: "Ready", alts: 'Alternatives', compare: "Compare", answer: 'Answer', result: 'Result'},
+		ru: {lang: "Язык", inputTitle: 'Введите альтернативы', add: 'Добавить', ready: 'Готово', alts: 'Альтернативы', compare: "Сравнение", answer: 'Ответить', result: 'Результат'},
+		ua: {lang: "Мова", inputTitle: 'Додайте альтернативи', add: 'Додати', ready: 'Готово', alts: 'Альтернативи', compare: "Порівняння", answer: 'Відповісти', result: 'Результат'}};
+	locale = this.locales.ua
+	alternatives = [/*{name: "Файловий менеджер", rate: 0}, {name: "Браузер", rate: 0}, {name: "Сапер", rate: 0}, {name: "Поштовий клієнт", rate: 0}, {name: "Калькулятор", rate: 0}*/];
+	alternativesList = [];
+	currentCompare = [];
+	currentRange = 5;
+	currentFirst = undefined;
+	currentIteration = 0;
+	outOfAlternatives = false;
+	newAlternative = '';
+	resultList = [];
+	ready = false;
+
+	getCurrentCompare = function(){
 			if(this.alternativesList.length > 1){
 				this.currentCompare = [];
 				if((this.currentIteration>=this.alternativesList.length) || this.currentFirst == undefined){
@@ -31,8 +40,8 @@ var app = new Vue({
 				this.outOfAlternatives = true
 				this.result()
 			}
-		},
-		answer: function(){
+		}
+		answer = function(){
 			if(!this.outOfAlternatives){
 				let values = [10-this.currentRange, this.currentRange*1];
 				for(let i = 0; i < this.alternatives.length; i++){
@@ -46,13 +55,13 @@ var app = new Vue({
 				this.getCurrentCompare()
 				this.currentRange = 5;
 			}
-		},
-		result: function(){
+		}
+		result = function(){
 			this.resultList = this.alternatives.sort(function(a,b){
 				return a.rate - b.rate
 			}).reverse()
-		},
-		rnd: function(min, max, prev) {
+		}
+		rnd = function(min, max, prev) {
 			if(prev != undefined){
 				let num = Math.floor(Math.random() * (max - min)) + min;
 				if(num != prev){
@@ -65,8 +74,8 @@ var app = new Vue({
 			}else{
 				return Math.floor(Math.random() * (max - min)) + min;
 			}
-		},
-		addAlternative: function(){
+		}
+		addAlternative = function(){
 			if(this.newAlternative != "" && this.newAlternative != " "){
 				this.alternatives.push({name: this.newAlternative, rate: 0})
 				this.newAlternative = '';
@@ -74,14 +83,10 @@ var app = new Vue({
 			else{
 				alert("Input correct alternative!")
 			}
-		},
-		getReady: function(){
+		}
+		getReady = function(){
 			this.alternativesList = JSON.parse(JSON.stringify(this.alternatives));
 			this.getCurrentCompare();
 			this.ready = true;
 		}
-	},
-	created: function(){
-		//....
-	}
-})
+}
